eabbb1b085d623d300af9a5d1d57e9234d812541
JewBabyBeats <110169149+JewBabyBeats@users.noreply.github.com>
2022-08-11 01:58:40 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xBce3Ed7Bf0cBd2e14A4cDa641881198055E37811";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
